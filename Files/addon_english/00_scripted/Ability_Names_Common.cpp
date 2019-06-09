66fb209bf92d902e1b1a2f22e89bd53afe9bea0f
SirWayNe <30441626+SirWayN3@users.noreply.github.com>
2019-06-09 13:21:56 +0200

    Sorting

--- Files/addon_english/00_scripted/Ability_Names_Common.cpp
+++ Files/addon_english/00_scripted/Ability_Names_Common.cpp
@@ -2,23 +2,29 @@
 	"DOTA_Tooltip_Ability_cf_INDEX_ab_RACE_crINDEX_INDEX_Description"			""
 	"DOTA_Tooltip_Ability_cf_INDEX_ab_RACE_crINDEX_INDEX_Lore"			""
 
+
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_destroy"			"Destroy this Building"
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_destroy_Description"			"Destroys this Building. Only Valid in Tools Mode"
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_destroy_Lore"			""
+
 
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_dummy"			"Dummy"
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_dummy_Description"			""
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_dummy_Lore"			""
 
+
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_toggleautocast"			"Toggle Autocast"
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_toggleautocast_Description"			""
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_toggleautocast_Lore"			""
+
 
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_upgrade"			"Upgrade Dummy"
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_upgrade_Description"			""
 	"DOTA_Tooltip_Ability_cf_00_common_ab_bu_upgrade_Lore"			""
 
+
 	"DOTA_Tooltip_Ability_cf_00_ab_common_treasurebox_income"			"Treasure Box gives Income."
 	"DOTA_Tooltip_Ability_cf_00_ab_common_treasurebox_income_Description"			""
 	"DOTA_Tooltip_Ability_cf_00_ab_common_treasurebox_income_Lore"			""
+
 
--- Files/addon_english/00_scripted/Building_Names.cpp
+++ Files/addon_english/00_scripted/Building_Names.cpp
@@ -1,34 +1,51 @@
 	"cf_fort_left"			"Fort Radiant"
 
+
 	"cf_fort_right"			"Fort Dire"
+
 
 	"cf_01_bu_01_barracks"			"Barracks"
 
+
 	"cf_01_bu_02_snipernest"			"Snipernest"
+
 
 	"cf_01_bu_03_weaponlab"			"Weaponlab"
 
+
 	"cf_01_bu_04_gryphonrock"			"Gryphon Rock"
+
 
 	"cf_01_bu_05_chapel"			"Chapel"
 
+
 	"cf_01_bu_06_hjordhejmen"			"Hjordhejmen"
+
 
 	"cf_01_bu_07_gjallerhorn"			"Gjallerhorn"
 
+
 	"cf_01_bu_08_artillery"			"Artillery"
+
 
 	"cf_01_bu_09_watchtower"			"Watchtower"
 
+
 	"cf_01_bu_10_heroicshrine"			"Heroic Shrine"
+
 
 	"cf_01_bu_11_stronghold"			"Stronghold"
 
+
 	"cf_01_bu_12_gunnershall"			"Gunner's Hall"
+
 
 	"cf_01_bu_13_marksmencamp"			"Marksmen Camp"
 
+
 	"cf_01_bu_14_church"			"Church"
 
+
 	"cf_00_bu_01_treasurechest"			"Treasure Chest"
+
 
